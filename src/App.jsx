f999984661a544cb16e7da59cd7e0367e61398a8
ecomter <72120650+ecomter@users.noreply.github.com>
2024-02-15 08:19:04 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,11 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Jinqiao Li",
+  title: ".NET Developer",
+  email: "t1472580369@outlook.com",
+  gitHub: "ecomter",
+  linkedIn: "li-jinqiao",
 };
 
 const primaryColor = "#4E567E";
